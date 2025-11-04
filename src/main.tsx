4245a8ddd008e267cc1cb30c8660d98a5f3535bf
Fazil S <abi.alif@acetvm.com>
2025-11-04 17:26:42 +0530

    New PC updated

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { AuthProvider } from '@/context/AuthContext'
+import { AuthProvider } from '@/context/AuthContext';
 
 
 createRoot(document.getElementById('root')!).render(
